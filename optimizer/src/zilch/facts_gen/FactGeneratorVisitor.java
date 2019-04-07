6c503884ff4831851a153e6c6d54e1419d4b760b
Dimitris Mouris <jimouris@udel.edu>
2019-04-07 19:51:36 -0400

    Add many more instructions to optimizer; todo: load and store

--- optimizer/src/zilch/facts_gen/FactGeneratorVisitor.java
+++ optimizer/src/zilch/facts_gen/FactGeneratorVisitor.java
@@ -37,8 +37,9 @@ public class FactGeneratorVisitor extends GJDepthFirst<String, String> {
     }
 
     public String visit(NodeSequence n, String argu) throws Exception {
-        if (n.size() == 1)
+        if (n.size() == 1) {
             return n.elementAt(0).accept(this,argu);
+        }
         String _ret = null;
         int _count=0;
         for ( Enumeration<Node> e = n.elements() ; e.hasMoreElements() ; ) {
@@ -62,16 +63,12 @@ public class FactGeneratorVisitor extends GJDepthFirst<String, String> {
     }
 
     /**
-    * f0 ->   ( ( Label() )* ( Stmt() )* )*
-    */
+     * f0 -> ( Stmt() )*
+     */
     public String visit(TinyRAMProg n, String argu) throws Exception {
         if (n.f0.present()) {
             for (int i = 0 ; i < n.f0.size() ; i++) {
                 String str = n.f0.elementAt(i).accept(this, argu);
-                // if (str.matches("(.*)ERR(.*)")){
-                //     ic2--;
-                //     continue;
-                // }
                 this.ic1++;
                 instrList.addLast(new Instruction_t("\""+argu+"\"", this.ic1, "\""+str+"\""));
             }
@@ -80,15 +77,17 @@ public class FactGeneratorVisitor extends GJDepthFirst<String, String> {
     }
 
     /**
-    * f0 -> NoOpStmt()
-    *       | ErrorStmt()
-    *       | CJumpStmt()
-    *       | JumpStmt()
-    *       | StoreWStmt()
-    *       | LoadWStmt()
-    *       | MovStmt()
-    *       | PrintStmt()
-    */
+     * f0 -> Label()
+     *       | TwoRegInstr()
+     *       | ThreeRegInstr()
+     *       | JmpStmts()
+     *       | StoreWStmt()
+     *       | LoadWStmt()
+     *       | PrintStmt()
+     *       | AnswerStmt()
+     *       | ReadStmt()
+     *       | SeekStmt()
+     */
     public String visit(Stmt n, String argu) throws Exception {
         this.ic2++;
         String stmt = n.f0.accept(this, argu);
@@ -96,38 +95,26 @@ public class FactGeneratorVisitor extends GJDepthFirst<String, String> {
     }   
 
     /**
-    * f0 -> "CJMP"
-    * f1 -> Register()
-    * f2 -> Label()
-    */
-    public String visit(CJumpStmt n, String argu) throws Exception {
-        // String tmp = n.f1.accept(this, argu);
-        // String label = n.f2.accept(this, argu);
-        // String op = "CJMP " + tmp + " " + label;
-        // cjumpList.addLast(new Cjump_t("\""+argu+"\"", this.ic2, "\""+label+"\""));
-        // varUseList.addLast(new VarUse_t("\""+argu+"\"", this.ic2, "\""+tmp+"\""));
-        // return op;
-        return null;
+     * f0 -> JmpOps()
+     * f1 -> Register()
+     * f2 -> Register()
+     * f3 -> Label()
+     */
+    public String visit(JmpStmts n, String argu) throws Exception {
+        String reg1 = n.f1.accept(this, argu);
+        String reg2 = n.f2.accept(this, argu);
+        String label = n.f3.accept(this, argu);
+        String op = "JMP " + reg1 + " " + reg2 + " " + label;
+        jumpList.addLast(new Jump_t("\""+argu+"\"", this.ic2, "\""+label+"\""));
+        return op;
     }
 
     /**
-    * f0 -> "JMP"
-    * f1 -> Label()
-    */
-    public String visit(JumpStmt n, String argu) throws Exception {
-        // String label = n.f1.accept(this, argu);
-        // String op = "JMP " + label;
-        // jumpList.addLast(new Jump_t("\""+argu+"\"", this.ic2, "\""+label+"\""));
-        // return op;
-        return null;
-    }
-
-    /**
-    * f0 -> "STOREW"
-    * f1 -> Register()
-    * f2 -> IntegerLiteral()
-    * f3 -> Register()
-    */
+     * f0 -> "STOREW"
+     * f1 -> Register()
+     * f2 -> Register()
+     * f3 -> SimpleExp()
+     */
     public String visit(StoreWStmt n, String argu) throws Exception {
         // String tmp1 = n.f1.accept(this, argu);
         // String lit = n.f2.accept(this, argu);
@@ -140,11 +127,11 @@ public class FactGeneratorVisitor extends GJDepthFirst<String, String> {
     }
 
     /**
-    * f0 -> "LOADW"
-    * f1 -> Register()
-    * f2 -> Register()
-    * f3 -> IntegerLiteral()
-    */
+     * f0 -> "LOADW"
+     * f1 -> Register()
+     * f2 -> Register()
+     * f3 -> SimpleExp()
+     */
     public String visit(LoadWStmt n, String argu) throws Exception {
         // String tmp1 = n.f1.accept(this, argu);
         // String tmp2 = n.f2.accept(this, argu);
@@ -157,143 +144,162 @@ public class FactGeneratorVisitor extends GJDepthFirst<String, String> {
     }
 
     /**
-    * f0 -> TwoRegInstrOp()
-    * f1 -> Register()
-    * f2 -> Register()
-    * f3 -> SimpleExp()
-    */
+     * f0 -> TwoRegInstrOp()
+     * f1 -> Register()
+     * f2 -> Register()
+     * f3 -> SimpleExp()
+     */
     public String visit(TwoRegInstr n, String argu) throws Exception {
         String op = n.f0.accept(this, argu);
         String dst = n.f1.accept(this, argu);
         String sec_reg = n.f1.accept(this, argu);
         String third_reg = n.f3.accept(this, argu);
         if (third_reg == null) { return null; }
-        String instr = null;
+        String instr = op + " " + dst + " " + sec_reg + " " + third_reg;
         if (third_reg.matches("r(.*)")) {
-            instr = "MOV " + dst + " " + sec_reg + " " + third_reg;
             varMoveList.addLast(new VarMove_t("\""+argu+"\"", this.ic2, "\""+dst+"\"", "\""+third_reg+"\""));
             varUseList.addLast(new VarUse_t("\""+argu+"\"", this.ic2, "\""+third_reg+"\""));
         } else if (third_reg.matches("[0-9]+")) {
-            instr = "MOV " + dst + " " + sec_reg + " " + Integer.parseInt(third_reg);
-            constMoveList.addLast(new ConstMove_t("\""+argu+"\"", this.ic2, "\""+dst+"\"", Integer.parseInt(third_reg)));
+            constMoveList.addLast(new ConstMove_t("\""+argu+"\"", this.ic2, "\""+dst+"\"", Integer.parseInt(third_reg) ));
         }
         varDefList.addLast(new VarDef_t("\""+argu+"\"", this.ic2, "\""+dst+"\""));
         return instr;
     }
-    
-    // /**
-    // * f0 -> Operator()
-    // * f1 -> Register()
-    // * f2 -> SimpleExp()
-    // */
-    // public String visit(BinOp n, String argu) throws Exception {
-    //     String op = n.f0.accept(this, argu);
-    //     String tmp = n.f1.accept(this, argu);
-    //     String exp = n.f2.accept(this, argu);
-    //     varUseList.addLast(new VarUse_t("\""+argu+"\"", this.ic2, "\""+tmp+"\""));
-    //     if (exp != null && exp.matches("r(.*)")) {
-    //         varUseList.addLast(new VarUse_t("\""+argu+"\"", this.ic2, "\""+exp+"\""));
-    //     }
-    //     return op + " " + tmp + " " + exp;
-    // }
 
     /**
-    * f0 -> ThreeRegInstrOp()
-    * f1 -> Register()
-    * f2 -> Register()
-    * f3 -> SimpleExp()
-    */
+     * f0 -> ThreeRegInstrOp()
+     * f1 -> Register()
+     * f2 -> Register()
+     * f3 -> SimpleExp()
+     */
     public String visit(ThreeRegInstr n, String argu) throws Exception {
         String op = n.f0.accept(this, argu);
         String dst = n.f1.accept(this, argu);
         String sec_reg = n.f2.accept(this, argu);
         String third_reg = n.f3.accept(this, argu);
         if (third_reg == null) { return null; }
-        String instr = null;
+        String instr = op + " " + dst + " " + sec_reg + " " + third_reg;
         varUseList.addLast(new VarUse_t("\""+argu+"\"", this.ic2, "\""+sec_reg+"\""));
-        
-        if (third_reg.matches("r(.*)")) {
-            instr = "ADD " + dst + " " + sec_reg + " " + third_reg;
-            // varMoveList.addLast(new VarMove_t("\""+argu+"\"", this.ic2, "\""+dst+"\"", "\""+third_reg+"\""));
+        if (third_reg.matches("r(.*)")) { // if third argument is not immediate
             varUseList.addLast(new VarUse_t("\""+argu+"\"", this.ic2, "\""+third_reg+"\""));
-            
-            binOpMoveList.addLast(new BinOpMove_t("\""+argu+"\"", this.ic2, "\""+dst+"\"", "\""+op+" "+sec_reg+" "+third_reg+"\""));
-        } else if (third_reg.matches("[0-9]+")) {
-            instr = "ADD " + dst + " " + sec_reg + " " + Integer.parseInt(third_reg);
-            // constMoveList.addLast(new ConstMove_t("\""+argu+"\"", this.ic2, "\""+dst+"\"", Integer.parseInt(third_reg)));
-            
-            binOpMoveList.addLast(new BinOpMove_t("\""+argu+"\"", this.ic2, "\""+dst+"\"", "\""+op+" "+sec_reg+" "+Integer.parseInt(third_reg)+"\""));
         }
+        binOpMoveList.addLast(new BinOpMove_t("\""+argu+"\"", this.ic2, "\""+dst+"\"", "\""+op+" "+sec_reg+" "+third_reg+"\""));
         varDefList.addLast(new VarDef_t("\""+argu+"\"", this.ic2, "\""+dst+"\""));
         return instr;
     }
 
     /**
-    * f0 -> "PRINT"
-    * f1 -> SimpleExp()
-    */
+     * f0 -> "PRINT"
+     * f1 -> Register()
+     * f2 -> Register()
+     * f3 -> Register()
+     */
     public String visit(PrintStmt n, String argu) throws Exception {
-        String exp = n.f1.accept(this, argu);
-        String op = "PRINT " + exp;
-        if (exp != null && exp.matches("r(.*)"))
-            varUseList.addLast(new VarUse_t("\""+argu+"\"", this.ic2, "\""+exp+"\""));
+        String reg = n.f3.accept(this, argu);
+        String op = "PRINT " + reg + " " + reg + " " + reg;
+        if (reg != null && reg.matches("r(.*)")) {
+            varUseList.addLast(new VarUse_t("\""+argu+"\"", this.ic2, "\""+reg+"\""));
+        }
+        return op;
+    }
+    
+    /**
+     * f0 -> "ANSWER"
+     * f1 -> Register()
+     * f2 -> Register()
+     * f3 -> Register()
+     */
+    public String visit(AnswerStmt n, String argu) throws Exception {
+        String reg = n.f3.accept(this, argu);
+        String op = "ANSWER " + reg + " " + reg + " " + reg;
+        if (reg != null && reg.matches("r(.*)")) {
+            varUseList.addLast(new VarUse_t("\""+argu+"\"", this.ic2, "\""+reg+"\""));
+        }
         return op;
     }
 
-    // /**
-    // * f0 -> "BEGIN"
-    // * f1 -> StmtList()
-    // * f2 -> "RETURN"
-    // * f3 -> SimpleExp()
-    // * f4 -> "END"
-    // */
-    // public String visit(StmtExp n, String argu) throws Exception {
-    //     n.f1.accept(this, argu);
-    //     this.ic2++;
-    //     String exp = n.f3.accept(this, argu);
-    //     String ret = "RETURN " + exp;
-    //     this.ic1++;
-    //     instrList.addLast(new Instruction_t("\""+argu+"\" ", this.ic1, "\""+ret+"\""));
-    //     if (exp != null && exp.matches("r(.*)")){
-    //         varUseList.addLast(new VarUse_t("\""+argu+"\"", this.ic2, "\""+exp+"\""));
-    //     }
-    //     return null;
-    // }
+    /**
+     * f0 -> "READ"
+     * f1 -> Register()
+     * f2 -> Register()
+     * f3 -> SimpleExp()
+     */
+    public String visit(ReadStmt n, String argu) throws Exception {
+        String dst = n.f1.accept(this, argu);
+        String third_reg = n.f3.accept(this, argu);
+        String op = "READ " + dst + " " + dst + " " + third_reg;
+        if (third_reg != null && third_reg.matches("r(.*)")) {
+            varUseList.addLast(new VarUse_t("\""+argu+"\"", this.ic2, "\""+third_reg+"\""));
+        }
+        return op;
+    }
+    
+    /**
+     * f0 -> "SEEK"
+     * f1 -> Register()
+     * f2 -> SimpleExp()
+     * f3 -> SimpleExp()
+     */
+    public String visit(SeekStmt n, String argu) throws Exception {
+        String dst = n.f1.accept(this, argu);
+        String sec_reg = n.f2.accept(this, argu);
+        String third_reg = n.f3.accept(this, argu);
+        String op = "SEEK " + dst + " " + sec_reg + " " + third_reg;
+        if (sec_reg != null && sec_reg.matches("r(.*)")) {
+            varUseList.addLast(new VarUse_t("\""+argu+"\"", this.ic2, "\""+sec_reg+"\""));
+        }
+        if (third_reg != null && third_reg.matches("r(.*)")) {
+            varUseList.addLast(new VarUse_t("\""+argu+"\"", this.ic2, "\""+third_reg+"\""));
+        }
+        return op;
+    }
 
     /**
-    * f0 ->   "MOV"
-    *       | "NOT"
-    */
+     * f0 -> "MOV"
+     *       | "NOT"
+     */
     public String visit(TwoRegInstrOp n, String argu) throws Exception {
         return n.f0.choice.toString();
     }
 
     /**
-    * f0 ->   "ADD"
-    *       | "SUB"
-    *       | "MULL" 
-    */
+     * f0 -> "AND"
+     *       | "OR"
+     *       | "XOR"
+     *       | "ADD"
+     *       | "SUB"
+     *       | "MULL"
+     *       | "SHL"
+     *       | "SHR"
+     */
     public String visit(ThreeRegInstrOp n, String argu) throws Exception {
         return n.f0.choice.toString();
     }
     
     /**
-    * f0 -> Register()
-    *       | IntegerLiteral()
-    *       | Label()
-    */
+     * f0 -> "JMP"
+     *       | "CJMP"
+     *       | "CNJMP"
+     */
+    public String visit(JmpOps n, String argu) throws Exception {
+        return n.f0.choice.toString();
+    }
+    
+    /**
+     * f0 -> Register()
+     *       | IntegerLiteral()
+     *       | Label()
+     */
     public String visit(SimpleExp n, String argu) throws Exception {
         return n.f0.accept(this, argu);
     }
 
     /**
-    * f0 -> <REGISTER>
-    */
+     * f0 -> <REGISTER>
+     */
     public String visit(Register n, String argu) throws Exception {
-        // String v = n.f0.choice.toString();
         String v = n.f0.toString();
         Var_t var = new Var_t("\""+argu+"\"", "\""+v+"\"");
-        
         for (Var_t variable : varList) {
             if (variable.temp.equals("\"" + v + "\"")) {
                 return v;
@@ -304,15 +310,15 @@ public class FactGeneratorVisitor extends GJDepthFirst<String, String> {
     } 
 
     /**
-    * f0 -> <INTEGER_LITERAL>
-    */
+     * f0 -> <INTEGER_LITERAL>
+     */
     public String visit(IntegerLiteral n, String argu) throws Exception {
         return n.f0.toString();
     }
 
     /**
-    * f0 -> <IDENTIFIER>
-    */
+     * f0 -> <IDENTIFIER>
+     */
     public String visit(Label n, String argu) throws Exception {
         return n.f0.toString();
     }
