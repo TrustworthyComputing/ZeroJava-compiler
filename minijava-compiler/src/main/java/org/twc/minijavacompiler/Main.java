c1a262eaec70a40c60f7969974123b93bb6ac6fd
Dimitris Mouris <jimouris@udel.edu>
2020-04-14 03:18:46 -0400

    Refactor main

--- minijava-compiler/src/main/java/org/twc/minijavacompiler/Main.java
+++ minijava-compiler/src/main/java/org/twc/minijavacompiler/Main.java
@@ -1,57 +1,91 @@
 package org.twc.minijavacompiler;
 
+import java.io.*;
+import java.util.Map;
+import java.util.ArrayList;
 import org.twc.minijavacompiler.minijavaparser.*;
 import org.twc.minijavacompiler.basetype.*;
 import org.twc.minijavacompiler.minijavasyntaxtree.*;
 import org.twc.minijavacompiler.symboltable.*;
 import org.twc.minijavacompiler.typecheck.*;
 import org.twc.minijavacompiler.zmipsgenerator.*;
-import java.io.*;
-import java.util.Map;
 
 public class Main {
-    public static void main (String [] args){
-        if (args.length < 1){
-            System.err.println("Usage: java Main <inputFile1> [<inputFile2>] ...");
-            System.exit(1);
+
+    private static boolean DEBUG = false;
+
+    public static void main (String[] args){
+        if (args.length == 0){
+            System.err.println("fatal error: no input files.");
+            System.exit(-1);
         }
-        FileInputStream fis = null;
-        int i = -1;
-        while (++i < args.length) {
+        ArrayList<String> input_files = new ArrayList<>();
+        for (String arg : args) {
+            if (arg.toUpperCase().equals("-DEBUG") || arg.toUpperCase().equals("--DEBUG")) {
+                DEBUG = true;
+            } else {
+                input_files.add(arg);
+            }
+        }
+        for (String arg : input_files) {
+            System.out.println("============================================================================");
+            System.out.println("Checking file '" + arg + "'");
+            FileInputStream fis = null;
             try {
-                fis = new FileInputStream(args[i]);
+                fis = new FileInputStream(arg);
                 MiniJavaParser parser = new MiniJavaParser(fis);
                 Goal root = parser.Goal();
                 VisitClasses firstvisit = new VisitClasses();
+                SymbolTableVisitor symtable_visit = null;
+                Map<String, Class_t> symbol_table = null;
                 try {
                     root.accept(firstvisit);
-                    SymbolTableVisitor secondvisit = new SymbolTableVisitor(firstvisit.getClassList());
-                    root.accept(secondvisit);
-                    Map<String, Class_t> symbol_table = secondvisit.getSymbolTable();
-                    // secondvisit.printSymbolTable();
+                    symtable_visit = new SymbolTableVisitor(firstvisit.getClassList());
+                    root.accept(symtable_visit);
+                    symbol_table = symtable_visit.getSymbolTable();
+                    if (DEBUG) {
+                        System.out.println();
+                        symtable_visit.printSymbolTable();
+                    }
                     TypeCheckVisitor type_checker = new TypeCheckVisitor(symbol_table);
                     root.accept(type_checker, null);
-                    // System.out.println("\nType Check Completed Successfully!\n");
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                    System.exit(-1);
+                }
+                System.out.println("[ \u2713 ] Type checking successful.");
+                System.out.println("============================================================================\n");
 
-                    ZMIPSGenVisitor generator = new ZMIPSGenVisitor(symbol_table, secondvisit.getGlobalsNumber());
+                PrintWriter writer = null;
+                try {
+                    ZMIPSGenVisitor generator = new ZMIPSGenVisitor(symbol_table, symtable_visit.getGlobalsNumber());
                     root.accept(generator, null);
-                    File fp = new File(args[i]);
+                    File fp = new File(arg);
                     String path = fp.getPath();
                     path = path.substring(0, path.lastIndexOf('.'));
-                    PrintWriter out = new PrintWriter(path + ".zmips");
-                    out.print(generator.getASM());
-                    System.out.print(generator.getASM());
-                    out.close();
+                    writer = new PrintWriter(path + ".zmips");
+                    writer.print(generator.getASM());
+                    if (DEBUG) {
+                        System.out.println(generator.getASM());
+                    }
+                    writer.close();
+
+                    System.out.println("============================================================================");
+                    System.out.println("[ \u2713 ] zMIPS code generated to " + path + ".zmips");
+                    System.out.println("============================================================================");
                 } catch (Exception ex) {
-                    System.out.println("\n\nAn error occurred: " + ex.getMessage() + "\n\n");
+                    ex.printStackTrace();
+                } finally {
+                    if (writer != null) writer.close();
                 }
-            } catch(Exception ex) {
+            } catch (ParseException ex) {
+                System.out.println(ex.getMessage());
+            } catch (FileNotFoundException ex) {
                 System.err.println(ex.getMessage());
             } finally {
                 try {
-                    if (fis != null)
-                        fis.close();
-                } catch(IOException ex) {
+                    if (fis != null) fis.close();
+                } catch (IOException ex) {
                     System.err.println(ex.getMessage());
                 }
             }
