cbfabe234afc89a01d31cd5f9419406c72ebedbd
Dimitris Mouris <jimouris@udel.edu>
2021-01-19 18:23:37 -0500

    Add public and private tape

--- src/test/resources/PopCount/PopCount.java
+++ src/test/resources/PopCount/PopCount.java
@@ -1,13 +1,15 @@
 class PopCount {
 
 	public static void main(String[] a) {
+		int threshold = PublicTape.read();
 		int num = PrivateTape.read();
 		int count = 0;
 	    while (num > 0) {
 			num &= num - 1;
 			count++;
 		}
-		Prover.answer(count);
+		System.out.println(count);
+		Prover.answer(count > threshold);
 	}
 
 }
